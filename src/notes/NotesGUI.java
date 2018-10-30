7a89b495f794ccb4aaba6930571049dfa630515c
spartai <spartaispartai@yahoo.com>
2018-10-30 15:59:34 +0100

    vmi

---
+++ src/notes/NotesGUI.java
@@ -0,0 +1,19 @@
+package notes;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class NotesGUI {
+    
+}
